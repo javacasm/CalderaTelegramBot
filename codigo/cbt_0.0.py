b46dadffc4b9f2c37f0c6986ab8b6e4aea2da8ec
javacasm <javacasm@gmail.com>
2019-11-26 14:13:51 +0100

    update

--- codigo/cbt_0.0.py
+++ codigo/cbt_0.0.py
@@ -4,9 +4,7 @@ from telegram.ext import Updater, CommandHandler
 def hello(update, context):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
-
-updater = Updater('1234567890:PON-AQUI-TU-TOKEN', use_context=True)
-
+updater = Updater('988090838:AAG2K9Pj2VJEDhJkx-0A58E1gWv4WmsziY8', use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 

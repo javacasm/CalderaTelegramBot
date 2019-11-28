d0b0b25e3365eaa79e1dd2d2a730e670b41ee19a
javacasm <javacasm@gmail.com>
2019-11-28 10:30:05 +0100

    update

--- codigo/cbt_0.0.py
+++ codigo/cbt_0.0.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Simple Bot to reply to Telegram messages.
+"""Simple Bot to reply to Telegram messages, get MQTT connection
 
-This is built on the API wrapper, see echobot2.py to see the same example built
-on the telegram.ext bot framework.
-This program is dedicated to the public domain under the CC0 license.
 """
+
+# Telegram staff: from inopya https://github.com/inopya/mini-tierra
 
 import logging
 import telegram
@@ -16,29 +15,41 @@ import paho.mqtt.client as mqtt # Import the MQTT library
 import time # The time library is useful for delays
 import datetime
 
+
+# Telegram Bot Authorization Token
+TELEGRAM_API_TOKEN = '12341234:PON-AQUI-TU-TOKEN'
+
 MQTT_SERVER = "192.168.1.200"
 
 update_id = None
 
+# 'keypad' buttons
 user_keyboard = [['/red','/blue'],['/green', '/black'],['/help','/free']]
 user_keyboard_markup = ReplyKeyboardMarkup(user_keyboard, one_time_keyboard=True)
-
-# Our "on message" event
-def messageFunction (client, userdata, message):
-    topic = str(message.topic)
-    message = str(message.payload.decode("utf-8"))
-    myLog(topic + ' - ' + message)
+commandList = '/red, /blue, /green, /black, /help, /free'
 
 def myLog(message):
     print(str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S.%f "))+ message)
+
+topic_sub = 'MeteoSalon'
+
+
+
+# Our "on message" event
+def checkMQTTSubscription (client, userdata, message):
+    topic = str(message.topic)
+    message = str(message.payload.decode("utf-8"))
+    #if topic.startswith(topic_sub):
+    #    pass ## TODO: check topics
+    myLog('MQTT:'+topic + ' - ' + message)
     
 def initMQTT():
     global ourClient
     ourClient = mqtt.Client("CBT_bot_mqtt") # Create a MQTT client object with this id
     ourClient.connect(MQTT_SERVER, 1883) # Connect to the test MQTT broker
     myLog('Conectado a MQTT broker '+MQTT_SERVER)
-    ourClient.subscribe("MeteoSalon/#") # Subscribe to the topic AC_unit
-    ourClient.on_message = messageFunction # Attach the messageFunction to subscription
+    ourClient.subscribe(topic_sub+'/#') # Subscribe to the topic 
+    ourClient.on_message = checkMQTTSubscription # Attach the messageFunction to subscription
     ourClient.loop_start() # Start the MQTT client
     myLog('MQTT client started')
 
@@ -48,17 +59,17 @@ def main():
     
     initMQTT()
     
-    # Telegram Bot Authorization Token
-    bot = telegram.Bot('12341234:PON-AQUI-TU-TOKEN')
-
-
+    bot = telegram.Bot(TELEGRAM_API_TOKEN)
+    
     # get the first pending update_id, this is so we can skip over it in case
     # we get an "Unauthorized" exception.
     try:
         update_id = bot.get_updates()[0].update_id
     except IndexError:
         update_id = None
-
+        
+    myLog('Init TelegramBot')
+    
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     last_pub = int(round(time.time() * 1000))
@@ -67,6 +78,7 @@ def main():
             now = int(round(time.time() * 1000))
             if (now - last_pub) > 5000: # 5 segundos
                 ourClient.publish("MeteoSalon/BotMQTTTest", "MQTT Bot") # Publish message to MQTT broker
+                myLog('Sent MQTT test')
                 last_pub = now
             updateBot(bot)
         except NetworkError:
@@ -78,10 +90,11 @@ def main():
             myLog('Excepcion!!')
 
 
-# mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-# FUNCIONES TELERAM
+# Telegram staff: from inopya https://github.com/inopya/mini-tierra
 # mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
 
+#URL de la API de TELEGRAM
+URL_API_TELEGRAM = "https://api.telegram.org/bot{}/".format(TELEGRAM_API_TOKEN)
 
 def get_url(url):
     '''
@@ -100,43 +113,53 @@ def send_message(text, chat_id):
     Funcion para enviar telergamas atraves de la API
     '''
     try:
-        url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+        url = URL_API_TELEGRAM + "sendMessage?text={}&chat_id={}".format(text, chat_id)
         #print("url >> ",url)
         get_url(url)
     except:
         print("ERROR de envio")
 
+# mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+
+
+# Update and chat with the bot
 def updateBot(bot):
-    """Echo the message the user sent."""
+    """Answer the message the user sent."""
     global update_id
+    myLog('Updating telegramBot')
     # Request updates after the last update_id
     for update in bot.get_updates(offset=update_id, timeout=10):
         update_id = update.update_id + 1
 
         if update.message:  # your bot can receive updates without messages
-            # Reply to the message
-            comando = update.message.text  #MENSAJE_RECIBIDO
-            chat_time = update.message.date
-            user = update.message.from_user #USER_FULL
+            # Proccess the incoming message
+            comando = update.message.text  # message text
+            command_time = update.message.date # command date
+            user = update.message.from_user #User full objetct
             chat_id = int(update.message.from_user.id)
             user_real_name = user.first_name #USER_REAL_NAME
-            print('Comand: '+comando)
+            myLog('Command: '+comando+' from user ' + str(user_real_name )+' in chat id:' + str(update.message.from_user.id)+ ' at '+str(command_time))
             if comando == '/start':
                 update.message.reply_text("Bienvenido al Bot casero v0.0", reply_markup=user_keyboard_markup)
             elif comando == 'hi':
                 update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
             elif comando == '/red':
                 ourClient.publish("MeteoSalon/ledRGB", "Red") # Publish message to MQTT broker
+                send_message ('Sent '+comando+'MQTT command', chat_id)
             elif comando == '/blue':
                 ourClient.publish("MeteoSalon/ledRGB", "Blue") # Publish message to MQTT broker
+                send_message ('Sent '+comando+'MQTT command', chat_id)
             elif comando == '/green':
                 ourClient.publish("MeteoSalon/ledRGB", "Green") # Publish message to MQTT broker
+                send_message ('Sent '+comando+'MQTT command', chat_id)
             elif comando == '/black':
                 ourClient.publish("MeteoSalon/ledRGB", "Black") # Publish message to MQTT broker
+                send_message ('Sent '+comando+'MQTT command', chat_id)
             elif comando == '/help':
-                send_message (listaComandosTxt, chat_id)
+                send_message (commandList, chat_id)
             elif comando == '/free':
                 ourClient.publish("MeteoSalon/free", "Free") # Publish message to MQTT broker
+                send_message ('Sent '+comando+'MQTT command', chat_id)                
             else:
                 update.message.reply_text('echobot: '+update.message.text)
 

b24bca5ab65308ba2699f8e047c036e06ffc2e2e
Javacasm <javacasm@gmail.com>
2020-01-09 14:34:11 +0100

    update

---
+++ codigo/httpUtils.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_url(url):
+    '''
+    Funcion de apoyo a la recogida de telegramas,
+    Recoge el contenido desde la url de telegram
+    '''
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content

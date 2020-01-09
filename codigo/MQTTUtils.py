117c5fd738368dc3fd092db75030f9c4f616353f
Javacasm <javacasm@gmail.com>
2020-01-09 15:02:31 +0100

    update

--- codigo/MQTTUtils.py
+++ codigo/MQTTUtils.py
@@ -12,7 +12,10 @@ def checkMQTTSubscription (client, userdata, message):
     message = str(message.payload.decode("utf-8"))
     MQTTData[topic] = [ utils.getStrDateTime() , message]
     utils.myLog('MQTT: '+utils.getStrDateTime() + ' ' +topic + ' - ' + message)
-    
+
+def on_log(client, userdata, level, buf):
+    utils.myLog("mqtt-log: ",buf)
+
 def initMQTT():
     ourClient = mqtt.Client("CBT_bot_mqtt") # Create a MQTT client object with this id
     ourClient.connect(config.MQTT_SERVER, 1883) # Connect to the test MQTT broker
@@ -22,3 +25,12 @@ def initMQTT():
     ourClient.loop_start() # Start the MQTT client
     utils.myLog('MQTT client started')
     return ourClient
+
+def publish(client,topic,message):
+    mqttResult = client.publish(topic,message)
+    utils.myLog(str(mqttResult))
+    utils.myLog('Sent MQTT: '+topic+ ' '+message)
+    if mqttResult.is_published():
+       utils.myLog('Publicado')
+    else:
+       utils.myLog('No publicado')  

025a3f254cbd46b8d55452d0f76fd5e3dca4bb03
Javacasm <javacasm@gmail.com>
2020-01-10 00:00:14 +0100

    update

--- codigo/Caldera.py
+++ codigo/Caldera.py
@@ -1,10 +1,13 @@
 
 import config
 import httpUtils
+import MQTTUtils
 
 URL_caldera = 'http://' + config.CALDERA_SERVER 
+MQTT_caldera = config.BaseTopic_sub + '/caldera'
+MQTT_caldera_Status = config.BaseTopic_sub + '/calderaStatus'
 
-def calderaOn():
+def calderaWebOn():
     urlOn = URL_caldera + '/?rele=on'
     # print(urlOn)
     response = httpUtils.get_url(urlOn)
@@ -12,10 +15,16 @@ def calderaOn():
     if 'Calefaccion: <strong>ON' in response:
         return 'ON'
 	
-def calderaOff():
+def calderaMQTTOn(client):
+    MQTTUtils.publish(client, MQTT_caldera, 'On')
+	
+def calderaWebOff():
     urlOff = URL_caldera + '/?rele=off'
     # print(urlOff)
     response = httpUtils.get_url(urlOff)
     # print(response)
     if 'Calefaccion: <strong>OFF' in response:
         return 'OFF'
+	
+def calderaMQTTOff(client):
+       MQTTUtils.publish(client, MQTT_caldera,'Off')

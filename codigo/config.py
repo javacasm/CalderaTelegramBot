6571dd95c6fc33fe1661b8ea4cccd5b9e889e160
Javacasm <javacasm@gmail.com>
2020-01-14 21:01:10 +0100

    update

--- codigo/config.py
+++ codigo/config.py
@@ -1,12 +1,14 @@
 # Configuration file
 
 # Telegram Bot Authorization Token
-TELEGRAM_API_TOKEN = '1005959855:AAFNsMCboucdaEi8awQlP_EuauPPJLtXptA'
+TELEGRAM_API_TOKEN = 'fasdfasdfasdfasdf'
 
 MQTT_SERVER = "192.168.1.100"
 
 BaseTopic_sub = 'MeteoSalon'
+topicCalderaStatus = BaseTopic_sub + b'/calderaStatus'
+topicLedRGB = BaseTopic_sub + b'/ledRGB'
 
 CALDERA_SERVER = "192.168.1.45"
 
-v = '1.1'
+v = '1.2'

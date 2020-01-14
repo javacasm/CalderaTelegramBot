d2c56fe1158290ffa86031f9dcebefca71a4f9d7
javacasm <javacasm@gmail.com>
2020-01-14 23:33:37 +0100

    update

--- codigo/config.py
+++ codigo/config.py
@@ -1,13 +1,16 @@
 # Configuration file
 
 # Telegram Bot Authorization Token
-TELEGRAM_API_TOKEN = 'fasdfasdfasdfasdf'
+
+TELEGRAM_API_TOKEN_JavaBot = 'sdfasdfasdf'
+TELEGRAM_API_TOKEN_VSCBOT = 'werqwerqwer'
+TELEGRAM_API_TOKEN = TELEGRAM_API_TOKEN_VSCBOT
 
 MQTT_SERVER = "192.168.1.100"
 
 BaseTopic_sub = 'MeteoSalon'
-topicCalderaStatus = BaseTopic_sub + b'/calderaStatus'
-topicLedRGB = BaseTopic_sub + b'/ledRGB'
+topicCalderaStatus = BaseTopic_sub + '/calderaStatus'
+topicLedRGB = BaseTopic_sub + '/ledRGB'
 
 CALDERA_SERVER = "192.168.1.45"
 

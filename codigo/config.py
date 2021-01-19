4c79aafc50a8c098f64583e632578098882b0762
javacasm <javacasm@gmail.com>
2021-01-19 08:16:28 +0100

    update

--- codigo/config.py
+++ codigo/config.py
@@ -2,18 +2,30 @@
 
 # Telegram Bot Authorization Token
 
-TELEGRAM_API_TOKEN = 'YOUR-TELEGRAM-TOKEN'
+
+TELEGRAM_API_TOKEN = 'PUT:HERE:YOUR:TOKEN'
 
 MQTT_SERVER = "192.168.1.88"
 
 BaseTopic_sub = 'MeteoSalon'
 topicCalderaStatus = BaseTopic_sub + '/calderaStatus'
 topicLedRGB = BaseTopic_sub + '/ledRGB'
+topic_subCalderaAction = BaseTopic_sub + '/caldera'
+topic_subInitConsola = BaseTopic_sub + '/initConsola'
+topic_subTemp = BaseTopic_sub + '/Temp'
+topic_subHum = BaseTopic_sub + '/Hum'
+topic_subPress = BaseTopic_sub + '/Press'
+topic_subData = BaseTopic_sub + '/SensorData'
+topic_subConsolaStatus = BaseTopic_sub + '/consolaStatus'
+topic_subBotTest = BaseTopic_sub + "/BotMQTTTest"
+
+msg_calderaOn = 'On'
+msg_calderaOff = 'Off'
 
 CALDERA_SERVER = "192.168.1.45"
 
-ADMIN_USER = 1111111111
+ADMIN_USER = 11111111111111111111
 
 ALLOWED_USERS = [ADMIN_USER]
 
-v = '1.2.5'
+v = '1.2.8'
